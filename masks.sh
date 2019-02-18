133ac02699ef6647dea1e8525f56b8e47bafbd66
Gaspard Jankowiak <gaspard.jankowiak@math.cnrs.fr>
2019-02-18 11:45:55 +0100

    better masks.sh

--- masks.sh
+++ masks.sh
@@ -3,15 +3,23 @@
 function genmask {
     conf=$1
     config=${conf/configs\//}
-    echo "Generating masks cache for '${config}'"
     # Skip configs that have no holes defined or that are symlinks to other configs
     holes_file="configs/${config}/holes.edp"
+
+    if [ -d "configs/${config}" -a -f "$holes_file" ];
+    then
+        echo "Generating masks cache for '${config}'"
+    else
+        echo "Config '${config}' is not available"
+        exit 1
+    fi
+
     [ ! -f "$holes_file" ] && continue
     [ -L "$holes_file" ] && continue
     masks_dir="configs/${config}/masks"
     [ -d "$masks_dir" ] || mkdir "$masks_dir"
     #for Nf in 2048 1024 512 256 128 16; do
-    for Nf in 512; do
+    for Nf in 128; do
         echo "Nf: $Nf"
         [ -f "$masks_dir/${Nf}.dat" ] && continue
         sed -e "s/#config#/$config/" -e "s/#Nf#/$Nf/" <masks.edp.in >masks.edp
